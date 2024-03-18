c27b1dff81a18108ac7898d643bb2f6e8eeed466
Sagar Gupta <Saaagarjii@gmail.com>
2024-03-19 00:56:13 +0530

    Backend User Functionality

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,22 @@
 import express from 'express';
 import dotenv from 'dotenv'
 import authRoutes from './routes/auth.routes.js'
+import connectToMongoDB from './db/ConnectToMongoDB.js';
+
+const app = express();
+const PORT = process.env.PORT || 5000
 
 dotenv.config();
-const PORT = process.env.PORT || 5000
-const app = express();
 
-app.listen(PORT,()=>console.log(`Server is running on ${PORT}`));
+app.use(express.json()); //to parse the incoming requests with JSON payloads from(req.body)
+
+app.use('/api/auth',authRoutes)
 
 app.get('/',(req,res)=>{
     res.send("Hello World!")
 });
 
-app.use('/api/auth',authRoutes)
+app.listen(PORT, () => {
+    connectToMongoDB();
+    console.log(`Server is running on ${PORT}`);
+});
