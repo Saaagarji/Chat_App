d4ffe9f4925bc41860d1617455776fa5b20bf62c
Sagar Gupta <Saaagarjii@gmail.com>
2024-03-19 20:00:05 +0530

    fix user_routes

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,9 +1,9 @@
 import express from'express'
-import protectRoute from '../middleware/protectRoute';
+import protectRoute from "../middleware/protectRoute";
 import { getUsersForSidebar } from '../controllers/user.controller';
 
 const router = express.Router();
 
-router.get("/",protectRoute,getUsersForSidebar)
+// router.get("/",protectRoute,getUsersForSidebar)
 
 export default router;
